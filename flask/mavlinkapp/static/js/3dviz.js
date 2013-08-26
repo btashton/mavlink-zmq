7771c83227fda24a9275403fc46b11aaf90880a3
Brennan Ashton <bashton@brennanashton.com>
2013-08-26 14:43:49 -0700

    add some graphing to imu page

--- flask/mavlinkapp/static/js/3dviz.js
+++ flask/mavlinkapp/static/js/3dviz.js
@@ -2,10 +2,12 @@
 c3dl.addMainCallBack(canvasMain, "tutorial");
 c3dl.addModel("/static/model/duck.dae");
 var duck;
+// Create a camera
+var cam = new c3dl.FreeCamera();
 
 var imu = {'roll':0,'pitch':0,'yaw':0}
-
-
+var imu_hist = {'roll':[0],'pitch':[0]'yaw':[0]}
+var maxpoints = 300
 // The program main
 function canvasMain(canvasName){
 
@@ -45,15 +47,14 @@ function canvasMain(canvasName){
  // Add the object to the scene
  scn.addObjectToScene(duck);
 
- // Create a camera
- var cam = new c3dl.FreeCamera();
+
 
  // Place the camera.
  // WebGL uses a right handed co-ordinate system.
  // move 200 to the right
  // move 300 up
  // move 500 units out
- cam.setPosition(new Array(200.0, 300.0, 500.0));
+ cam.setPosition(new Array(000.0, 000.0, 500.0));
 
  // Point the camera.
  // Here it is pointed at the same location as
@@ -76,6 +77,17 @@ function update_pos(msg)
 	imu.roll = msg.roll
 	imu.yaw = msg.yaw
 	imu.pitch = msg.pitch
+	if imu_hist.roll.length >= maxpoints
+	{
+		imu_hist.roll.slice(1)
+		imu_hist.yaw.slice(1)
+		imu_hist.pitch.slice(1)
+	}
+	imu_hist.roll.push(msg.roll)
+	imu_hist.yaw.push(msg.yaw)
+	imu_hist.pitch.push(msg.pitch)
+
+	update_plot();
 }
 
 
@@ -99,3 +111,17 @@ $(function() {
 		}
     });
 });
+
+
+// setup plot
+var options = {
+	series: { shadowSize: 0 }, // drawing is faster without shadows
+	yaxis: { min: 0, max: 100 },
+	xaxis: { show: false }
+};
+var plot = $.plot($("#imuplot"), imu_hist.roll, options);
+
+function updateplot() {
+	plot.setData(imu_hist.roll);
+	plot.draw();
+}
